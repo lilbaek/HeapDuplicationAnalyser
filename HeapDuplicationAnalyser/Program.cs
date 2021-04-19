d1fa72b8ba1814aff7012154c6bd22997d7fb782
Morten <mortenlilbaek@gmail.com>
2021-04-19 12:03:03 +0200

    Initial commit with code

---
+++ HeapDuplicationAnalyser/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.Diagnostics.Runtime;
+
+namespace HeapDuplicationAnalyser
+{
+    class Program
+    {
+        private static readonly Dictionary<string, int> DuplicatedInstances = new Dictionary<string, int>();
+
+        static void Main(string[] args)
+        {
+            var filePath = args[0];
+            if (File.Exists(filePath) == false)
+            {
+                Console.WriteLine($"{filePath} - does not exist.");
+                return;
+            }
+
+            Console.WriteLine("Loading dump");
+            using var dataTarget = DataTarget.LoadDump(filePath);
+            Console.WriteLine("Dump loaded - analyzing");
+            foreach (var clr in dataTarget.ClrVersions)
+            {
+                using var runtime = clr.CreateRuntime();
+                var heap = runtime.Heap;
+                foreach (var obj in heap.EnumerateObjects())
+                {
+                    AnalyseDuplications(obj, heap);
+                }
+            }
+
+            var orderByDescending = DuplicatedInstances.OrderByDescending(x => x.Value).Take(100).ToList();
+            foreach (var pair in orderByDescending)
+            {
+                Console.WriteLine("Times duplicated " + pair.Value + " - value: " + pair.Key);
+            }
+        }
+
+        private static void AnalyseDuplications(ClrObject obj, ClrHeap heap)
+        {
+            if (obj.IsNull)
+                return;
+            if (obj.Type != heap.StringType)
+                return;
+            var asString = obj.AsString();
+            if (string.IsNullOrEmpty(asString))
+                return;
+            if (!DuplicatedInstances.ContainsKey(asString))
+            {
+                DuplicatedInstances.Add(asString, 1);
+            }
+            else
+            {
+                var duplicatedInstance = DuplicatedInstances[asString];
+                DuplicatedInstances[asString] = duplicatedInstance + 1;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # HeapDuplicationAnalyser
+
+Small tool to help find duplicated strings in memory dumps. If you have many duplicated strings you might considering using String.Intern as an optimization point to save memory.
+I created this tool as I am working with big dumps (20 GB in size) and DotMemory is unable to open the dumps due to "Out Of Memory". 
+The tool will print the top 100 strings with most duplications.
+
+## Usage Instructions
+
+1. Create a **Full** Process Dump (e.g. in Task Manager, right-click on the process and click **Create Dump File**)
+1. Run the tool by executing the command line HeapDuplicationAnalyser.exe <Full path of the Process Dump>
+
+## Sample output
+
+Loading dump
+Dump loaded - analyzing
+Times duplicated 823435 - value: PROD
+Times duplicated 815731 - value: DEV
+Times duplicated 781543 - value: SAND
+Times duplicated 781504 - value: STAGING
+Times duplicated 781501 - value: UN
+Times duplicated 677617 - value: Server=localhost\sql;Database=LOCAL;UID=LOCAL;MultipleActiveResultSets=True;
+Times duplicated 566714 - value: DK
+Times duplicated 554733 - value: US
+Times duplicated 553422 - value: GB
+Times duplicated 550399 - value: DE
+Times duplicated 524179 - value: HR
+Times duplicated 480995 - value: 0
+Times duplicated 443097 - value: PR
+Times duplicated 213066 - value: 1
+Times duplicated 134877 - value: 3
+Times duplicated 133046 - value: S
+Times duplicated 128589 - value: K
+Times duplicated 102580 - value: 2
